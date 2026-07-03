7aeb97e56ce03a71fe1de45216ef4539db794823
pixemisti <braimahayaan@gmail.com>
2026-07-03 20:05:54 +0000

    added crt and changed player sprite

--- game/autoloads/Util.gd
+++ game/autoloads/Util.gd
@@ -1,6 +1,12 @@
 extends Node
 
+const GRID_SIZE: int = 14
+const GRID_BOUNDS: Rect2i = Rect2i(0, 0, GRID_SIZE, GRID_SIZE)
+
+@export var grid_offset: int = 8
 @export var tile_size: int = 16
+
+var occupied: PackedVector2Array = PackedVector2Array()
 
 func get_closest_in_group(position: Vector2, group: StringName) -> Node2D:
 	var members: Array = get_tree().get_nodes_in_group(group)
@@ -19,5 +25,51 @@ func get_closest_in_group(position: Vector2, group: StringName) -> Node2D:
 	
 	return closest_candidate
 
-func get_global_from_tile_position(pos: Vector2i) -> Vector2:
-	return Vector2(pos * tile_size) - (Vector2.ONE * tile_size * 0.5)
+func get_global_from_tile_position(pos: Vector2i, centered: bool = true) -> Vector2:
+	var new_pos: Vector2 = Vector2(pos * tile_size) + (Vector2.ONE * grid_offset)
+	if centered:
+		new_pos += Vector2.ONE * tile_size / 2
+	new_pos += Vector2.ONE
+	return new_pos
+
+func get_tile_from_global_position(pos: Vector2, centered: bool = true) -> Vector2:
+	var new_pos: Vector2 = pos - (Vector2.ONE * grid_offset)
+	if centered:
+		new_pos -= Vector2.ONE * tile_size * 0.5
+	new_pos /= tile_size
+	return new_pos
+
+func occupy_tile(tile_pos: Vector2) -> void:
+	if not occupied.has(tile_pos):
+		occupied.append(tile_pos)
+
+func unoccupy_tile(tile_pos: Vector2) -> void:
+	if occupied.has(tile_pos):
+		occupied.erase(tile_pos)
+
+func get_random_occupied_tile() -> Vector2:
+	if occupied.size() > 0:
+		return occupied[randi() % occupied.size()]
+	else:
+		return Vector2.INF
+
+func get_random_unoccupied_tile() -> Vector2:
+	var total_slots = (GRID_SIZE + 1) ** 2
+	var available_indices: PackedInt32Array = PackedInt32Array()
+	
+	var occupied_indices = {}
+	for tile in occupied:
+		var idx = int((tile.y * 15) + tile.x)
+		occupied_indices[idx] = true
+	
+	for i in range(total_slots):
+		if not occupied_indices.has(i):
+			available_indices.append(i)
+	
+	if available_indices.is_empty():
+		return Vector2(-1, -1)
+	
+	var choice = available_indices[randi() % available_indices.size()]
+	var x = choice % 15
+	var y = choice / 15
+	return Vector2(x, y)
--- game/player/player.gd
+++ game/player/player.gd
@@ -9,17 +9,36 @@ const INPUTS = {
 }
 
 @export var coll_detect_raycast: RayCast2D
+@export var sprite: Sprite2D
 
 var moving: bool = false
 
+func _ready() -> void:
+	pass
+
 func move(dir: Vector2, move_time: float = 0.1):
+	sprite.global_rotation = Vector2.RIGHT.angle_to(dir)
 	coll_detect_raycast.target_position = dir * Util.tile_size
 	coll_detect_raycast.force_raycast_update()
 	if not coll_detect_raycast.is_colliding():
-		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+		tween.set_parallel()
 		tween.tween_property(
 			self, "global_position", global_position + (dir * Util.tile_size), move_time
 		)
+		tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(
+			sprite, "scale:y", 1, move_time
+		).from(0.25)
+		moving = true
+		await tween.finished
+		moving = false
+	else:
+		var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+		tween.set_parallel()
+		tween.tween_property(
+			sprite, "offset", Vector2.ZERO, move_time
+		).from(Vector2.RIGHT * Util.tile_size * 0.25)
 		moving = true
 		await tween.finished
 		moving = false
